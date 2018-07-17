a5637d28beaa46c9de09ac912c6eaaa96142859b
Allyson Aberg <allya7897@gmail.com>
2018-07-17 19:55:59 -0400

    adding in scene transition code but duplicates omg

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -10,7 +10,8 @@ public class cameraController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        DontDestroyOnLoad(transform.gameObject);
+
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -14,6 +14,8 @@ public class playerController : MonoBehaviour {
 	void Start () {
         anim = GetComponent<Animator>();
         playerRigidBody = GetComponent<Rigidbody2D>();
+
+        DontDestroyOnLoad(transform.gameObject);
 	}
 	
 	// Update is called once per frame
@@ -22,14 +24,12 @@ public class playerController : MonoBehaviour {
         playerMoving = false;
 
         if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) {
-            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
             playerRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*moveSpeed, playerRigidBody.velocity.y);
             playerMoving = true;
             lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) {
-            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
             playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, Input.GetAxisRaw("Vertical")*moveSpeed);
             playerMoving = true;
             lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
